dd8f314fd251bbc7c2cc434428656523fa751d85
Aurora Santos <etheraurora1@gmail.com>
2024-09-03 12:12:55 -0300

    Completo o desafio, e criado um menu simples

--- Program.cs
+++ Program.cs
@@ -13,10 +13,31 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite();
+Console.WriteLine("Qual quarto?");
+string quarto = Console.ReadLine().ToLower();
+switch (quarto){
+    case "standard":
+    suite = new Suite(tipoSuite: "Standard", capacidade: 2, valorDiaria: 30);
+    break;
+    case "deluxe":
+    suite = new Suite(tipoSuite: "Deluxe", capacidade: 4, valorDiaria: 60);
+    break;
+    case "premium":
+    suite = new Suite(tipoSuite: "Premium", capacidade: 6, valorDiaria: 90);
+    break;
+    case "executiva":
+    suite = new Suite(tipoSuite: "Executiva", capacidade: 8, valorDiaria: 120);
+    break;
+    default:
+    Console.WriteLine("Categoria não valida, por favor tente novamente");
+    break;
+}
+Console.WriteLine("Quantos dias?");
+int dias = Convert.ToInt32(Console.ReadLine());
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: dias);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
